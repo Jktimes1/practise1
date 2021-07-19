18e8d4ab77751f051fa640c7148b5f3075cd1dcd
navni99 <chauhannavni22@gmail.com>
2021-07-19 12:22:11 +0530

    two way binding

--- Day11/user-app/src/components/Userform.js
+++ Day11/user-app/src/components/Userform.js
@@ -1,9 +1,13 @@
 import { useState } from "react"
 export default function Userform(){
     const [userform,setUserform]=useState({firstname:"Ram"});
+    const handleEvent= function (event){
+        console.log(event);
+        setUserform({...userform, firstname:event.target.value})
+    }
     return(
         <div><h3>Create User</h3>
-        <input value={userform.firstname} type="text" />
+        <input value={userform.firstname} onChange={handleEvent} />
 
         </div>
     )
