2ebfda1abd9a94b970d0d51f7718a558759cd204
navni99 <chauhannavni22@gmail.com>
2021-07-19 15:38:18 +0530

    using axios to post to db.json

--- Day11/user-app/src/components/Userform.js
+++ Day11/user-app/src/components/Userform.js
@@ -1,13 +1,18 @@
 import { useState } from "react"
+import axios from "axios"
 export default function Userform(){
     const [userform,setUserform]=useState({firstname:"Ram",age:'20'});
     const handleEvent= function (event){
-        console.log(event);
+        //console.log(event);
         setUserform({...userform, [event.target.name]:event.target.value});
     }
     const save = function(event){
-        console.log(event);
+        //console.log(event);
         console.log(userform.firstname+userform.age);
+        const promise=axios.post("http://localhost:4200/users",userform);
+        promise.then(function(response){
+            console.log(response);
+        })
     }
     return(
         <div><h3>Create User</h3>
