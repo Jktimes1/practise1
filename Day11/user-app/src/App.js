2aca4a2f7ed0d9976f794a9a420f0b691f13cce1
navni99 <chauhannavni22@gmail.com>
2021-07-19 12:07:03 +0530

    c1

--- Day11/user-app/src/App.js
+++ Day11/user-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Userform from './components/Userform';
 function App() {
   return (
     <div className="App">
+            <Userform/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ Day11/user-app/src/components/Userform.js
@@ -0,0 +1,10 @@
+import { useState } from "react"
+export default function Userform(){
+    const [userform,setUserform]=useState({firstname:"Ram"});
+    return(
+        <div><h3>Create User</h3>
+        <input value={userform.firstname} type="text" />
+
+        </div>
+    )
+}
