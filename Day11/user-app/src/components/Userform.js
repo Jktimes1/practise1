191fd823c16d3cc00d58b8a3f1086562a1513d10
navni99 <chauhannavni22@gmail.com>
2021-07-19 12:43:03 +0530

    added age field

--- Day11/user-app/src/components/Userform.js
+++ Day11/user-app/src/components/Userform.js
@@ -1,14 +1,23 @@
 import { useState } from "react"
 export default function Userform(){
-    const [userform,setUserform]=useState({firstname:"Ram"});
+    const [userform,setUserform]=useState({firstname:"Ram",age:'20'});
     const handleEvent= function (event){
         console.log(event);
         setUserform({...userform, firstname:event.target.value})
     }
+    const handleEventage= function (event){
+        console.log(event);
+        setUserform({...userform, age:event.target.value})
+    }
+    const save = function(event){
+        console.log(event);
+        console.log(userform.firstname+userform.age);
+    }
     return(
         <div><h3>Create User</h3>
         <input value={userform.firstname} onChange={handleEvent} />
-
+        <input value={userform.age} onChange={handleEventage}></input>
+        <button onClick={save}>Save</button>
         </div>
     )
 }
