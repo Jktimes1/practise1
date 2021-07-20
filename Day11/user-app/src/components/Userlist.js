cb2c0e3255e0b0264221c226331d7919c385074c
navni99 <chauhannavni22@gmail.com>
2021-07-20 11:00:10 +0530

    get from database

--- Day11/user-app/src/components/Userlist.js
+++ Day11/user-app/src/components/Userlist.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 export default function Userlist(){
     const[users,setUsers]=useState([]);
+    useEffect(function(){
+        if(users.length!=0){
+            return;
+        }
+        console.log('Caleeddd');
+        const promise = axios.get('http://localhost:4200/users')
+        promise.then(function(response){
+            setUsers(response.data)
+            console.log(response.data);
+
+        })
+    })
     return(
         <div>
             <table className='table table-bordered table-hover table-responsive'>
                 <thead>
-                    <tr><td>Firstname</td>
-                        <td>Age</td>
+                    <tr><th>Firstname</th>
+                        <th>Age</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>Navni</td><td>22</td>
+                    {users.map(function(user,index){
+                    return <tr>
+                    <td>{user.firstname}</td><td>{user.age}</td>
                     </tr>
+                })}
                 </tbody>
             </table>
         </div>
