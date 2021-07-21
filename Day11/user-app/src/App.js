c6bfd1e0eb9f792951aa994c5b29a8bc026c43b2
navni99 <chauhannavni22@gmail.com>
2021-07-21 15:40:49 +0530

    Dynamice drop down

--- Day11/user-app/src/App.js
+++ Day11/user-app/src/App.js
@@ -9,13 +9,46 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-            <Userform/>
-            <Userlist/>
+            {/* <Userform/>
+            <Userlist/> */}
+            
+<Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <Userform />
+          </Route>
+          <Route path="/users">
+            <Userlist /> <Userform/>
+          </Route>
+          {/* <Route path="/">
+            < />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
     </div>
-     
+       
 
   );
 }
--- Day11/user-app/src/components/Userform.js
+++ Day11/user-app/src/components/Userform.js
@@ -1,12 +1,23 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import axios from "axios"
 import Message from "./Message";
 export default function Userform(){
+    const[skills,setSkills]=useState([]);
     const [userform,setUserform]=useState({
         firstname:"Ram",
         age:'20',
         date:'2021-07-20',
-        skill:'Html',
+        
+    });
+    useEffect(function(){
+        // if(skills.length!==0){
+        //     return;
+        // }
+        const promise=axios.get('http://localhost:4200/skills');
+        promise.then(function(response){
+            setSkills(response.data);
+            console.log(response.data);
+        },[])
     });
     const [isError,setError]=useState({msg:'default message'});
     const handleEvent= function (event){
@@ -38,11 +49,16 @@ export default function Userform(){
         <label>Name</label><input name='firstname' className='form-control'value={userform.firstname} onChange={handleEvent} />
         <label >Age</label><input name='age' value={userform.age} className='form-control' onChange={handleEvent} type="Number"></input>
         <label >Date </label><input name='date' value={userform.date} className='form-control' onChange={handleEvent} type="date" />
-        <select name='skill' onChange={handleSection}>
-            <option>Select a skill</option>
+        {/* <select name='skill' onChange={handleSection}>
+            <option defaultValue>Select a skill</option>
             <option value='Html'>Html</option>
             <option value='css'>css</option>
             <option value='JS'>JS</option>
+        </select>   THESE ARE HARDCODED VALUES*/} 
+        <select name='skill' onChange={handleEvent}>
+        {skills.map(function(skill,index){
+                    return (
+                    <option key={index} value={skill}>{skill}</option>)})};
         </select>
         <button className='btn-dark'onClick={save}>Save</button>
         <Message msg={userform.msg}></Message>
